a3f31eaebb3a47f04b81f5e0ed301a10f654015f
Lightman <yaruk.my@gmail.com>
2020-11-22 19:31:56 +0200

    add 'Property' service to the current container

---
+++ tests/ProviderTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\Property\Tests;
+
+use Mrself\Container\Registry\ContainerRegistry;
+use Mrself\Property\Property;
+use Mrself\Property\PropertyProvider;
+use PHPUnit\Framework\TestCase;
+
+class ProviderTest extends TestCase
+{
+    public function testPropertyServiceExistsInContainer()
+    {
+        ContainerRegistry::reset();
+        $propertyProvider = PropertyProvider::make();
+        $propertyProvider->register();
+
+        $propertyContainer = ContainerRegistry::get('Mrself\Property');
+        $property = $propertyContainer->get(Property::class);
+        $this->assertInstanceOf(Property::class, $property);
+    }
+}
